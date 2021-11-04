b24eafe7b711369cace90f66f2a0fb58c516bf0b
HershGupta25 <67029903+HershGupta25@users.noreply.github.com>
2021-11-04 02:13:49 -0400

    Add files via upload

---
+++ OKC.R
@@ -0,0 +1,22 @@
+library(readr)
+library(dplyr)
+library(tidyverse)
+
+setwd("/Users/HerschelGupta/Downloads");
+shots <- read_csv("shots_data.csv");
+
+shot_bins <- shots %>% 
+         mutate(shottype = case_when(
+          (abs(x) > 22 & y <= 7.8) ~ "Corner 3",
+          ((x^2 + y^2)^.5 > 23.75) ~ "Non Corner 3",
+          TRUE ~ "Two Point")) %>%
+          group_by(team,shottype)
+
+summary <- shot_bins %>%
+            group_by(team, shottype) %>%
+            summarize(made = sum(fgmade),n=n()) %>%
+            mutate("eFG%" = round(if_else(grepl("3",shottype), 
+                                       (made*1.5)/n,made/n),3),
+                   "Shot %" = round(n/sum(n),3))
+
+
