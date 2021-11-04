8ecaf64289a0673282d5151f1501e84fd9e61edc
HershGupta25 <67029903+HershGupta25@users.noreply.github.com>
2021-11-04 02:15:08 -0400

    Add files via upload
    Code for OKC DS&S Technical Assessment.

--- OKC.R
+++ OKC.R
@@ -9,8 +9,7 @@ shot_bins <- shots %>%
          mutate(shottype = case_when(
           (abs(x) > 22 & y <= 7.8) ~ "Corner 3",
           ((x^2 + y^2)^.5 > 23.75) ~ "Non Corner 3",
-          TRUE ~ "Two Point")) %>%
-          group_by(team,shottype)
+          TRUE ~ "Two Point")) 
 
 summary <- shot_bins %>%
             group_by(team, shottype) %>%
